33956a83db3202c1f02848914f50d4746f0403e9
Emanoel Lopes <emanoel.lopes.web@gmail.com>
2015-04-29 14:20:11 -0300

    Adicionando os arquivos ao repositorio

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>RA Intercement</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <!-- Styles --> 
+        <link rel="stylesheet" href="src/styles/normalize.css">
+        <link rel="stylesheet" href="src/styles/main.css">
+        
+        <!-- Head Scripts --> 
+        <script src="src/scripts/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>s
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+        <!-- Add your site or application content here -->
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+
+        <!-- Footer Scripts -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="src/scripts/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <script src="src/scripts/plugins.js"></script>
+        <script src="src/scripts/main.js"></script>
+        <!--/ Footer Scripts -->
+
+        <!-- GA -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+                                                                    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+                                   e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+                                   e.src='https://www.google-analytics.com/analytics.js';
+                                   r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+        <!--/ GA -->
+    </body>
+</html>
